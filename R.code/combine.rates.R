82c70501d03a4cf1e0616f326f36979832d9188a
samirsoneji <samirsoneji@SSoneji-2.local>
2014-12-11 13:09:20 -0500

    edited code

--- R.code/combine.rates.R
+++ R.code/combine.rates.R
@@ -53,8 +53,7 @@ load("~/Cancer/data/prop.leukemia.Rdata")
 library(foreign)
 library(survival)
 
-setwd("~/Dropbox/Cancer/Value")
-source("R.code/lifetable.R")
+source("~/Cancer/R.code/lifetable.R")
 
 decomp.fxn <- function(mx, prop, cancer, year.list) {
     value.list <- c("ex.overall","ex.localized","ex.regional","ex.distant","prop.localized","prop.regional","prop.distant")
@@ -66,30 +65,40 @@ decomp.fxn <- function(mx, prop, cancer, year.list) {
 
     if (sex=="single" & cancer!="prostate") {
         for (y in 1:length(year.list)) {
-            ex.localized <- lifetab.grad.nax.nLxappr(nMx=c(rep(0,9),mx[,as.character(year.list[y]),"1. localized"]),a0=0.1385, Rdx=1)$ex[10]
-            ex.regional <- lifetab.grad.nax.nLxappr(nMx=c(rep(0,9),mx[,as.character(year.list[y]),"2. regional"]),a0=0.1385, Rdx=1)$ex[10]
-            ex.distant <- lifetab.grad.nax.nLxappr(nMx=c(rep(0,9),mx[,as.character(year.list[y]),"4. distant"]),a0=0.1385, Rdx=1)$ex[10]
-            prop.localized <- prop[as.character(year.list[y]),"1. localized"]
-            prop.regional <- prop[as.character(year.list[y]),"2. regional"]
-            prop.distant <- prop[as.character(year.list[y]),"4. distant"]
-            ex.overall <-ex.localized*prop.localized+ex.regional*prop.regional+ex.distant*prop.distant
-            datos[y,"ex.overall"] <- ex.overall
-            datos[y,"ex.localized"] <- ex.localized
-            datos[y,"ex.regional"] <- ex.regional
-            datos[y,"ex.distant"] <- ex.distant
-            datos[y,"prop.localized"] <- prop.localized
-            datos[y,"prop.regional"] <- prop.regional
-            datos[y,"prop.distant"] <- prop.distant
-        }
+          mx.localized <- c(rep(0,9),mx[,as.character(year.list[y]),"1. localized"])
+          mx.regional <- c(rep(0,9),mx[,as.character(year.list[y]),"2. regional"])
+          mx.distant <- c(rep(0,9),mx[,as.character(year.list[y]),"4. distant"])
+          mx.localized[which(is.na(mx.localized)==TRUE)] <- 0
+          mx.regional[which(is.na(mx.regional)==TRUE)] <- 0
+          mx.distant[which(is.na(mx.distant)==TRUE)] <- 0 
+          ex.localized <- lifetab.grad.nax.nLxappr(nMx=mx.localized,a0=0.1385, Rdx=1)$ex[10]
+          ex.regional <- lifetab.grad.nax.nLxappr(nMx=mx.regional,a0=0.1385, Rdx=1)$ex[10]
+          ex.distant <- lifetab.grad.nax.nLxappr(nMx=mx.distant,a0=0.1385, Rdx=1)$ex[10]
+          prop.localized <- prop[as.character(year.list[y]),"1. localized"]
+          prop.regional <- prop[as.character(year.list[y]),"2. regional"]
+          prop.distant <- prop[as.character(year.list[y]),"4. distant"]
+          ex.overall <-ex.localized*prop.localized+ex.regional*prop.regional+ex.distant*prop.distant
+          datos[y,"ex.overall"] <- ex.overall
+          datos[y,"ex.localized"] <- ex.localized
+          datos[y,"ex.regional"] <- ex.regional
+          datos[y,"ex.distant"] <- ex.distant
+          datos[y,"prop.localized"] <- prop.localized
+          datos[y,"prop.regional"] <- prop.regional
+          datos[y,"prop.distant"] <- prop.distant
+        }}
+    
         datos <- data.frame(datos)
-        
-#Samir: I changed the output so it now spits 3 values for change.stage and change.ex corresponding to each cancer stage
-decomp.fxn <- function(datos, year1, year2) {
-    tmp <- datos[as.character(c(year1,year2)),]
-    ex.overall.diff <- diff(tmp$ex.overall)
-    change.stage <- c(diff(tmp$prop.localized)*mean(tmp$ex.localized), diff(tmp$prop.regional)*mean(tmp$ex.regional),diff(tmp$prop.distant)*mean(tmp$ex.distant)) 
-    change.ex <- c(diff(tmp$ex.localized)*mean(tmp$prop.localized), diff(tmp$ex.regional)*mean(tmp$prop.regional), diff(tmp$ex.distant)*mean(tmp$prop.distant))
-    return(list(ex.overall.diff=ex.overall.diff, change.stage=change.stage, change.ex=change.ex))
+
+    for (y in 1:(length(year.list)-1)) {
+      year1 <- year.list[y]
+      year2 <- year.list[y+1]
+      tmp <- datos[as.character(c(year1,year2)),]
+      ex.overall.diff <- diff(tmp$ex.overall)
+      change.stage <- c(diff(tmp$prop.localized)*mean(tmp$ex.localized), diff(tmp$prop.regional)*mean(tmp$ex.regional),diff(tmp$prop.distant)*mean(tmp$ex.distant)) 
+      change.ex <- c(diff(tmp$ex.localized)*mean(tmp$prop.localized), diff(tmp$ex.regional)*mean(tmp$prop.regional), diff(tmp$ex.distant)*mean(tmp$prop.distant))
+      
+
+      return(list(ex.overall.diff=ex.overall.diff, change.stage=change.stage, change.ex=change.ex))
 }
 print(decomp.fxn(datos, 1981, 1991))
 print(decomp.fxn(datos, 1991, 2001))
