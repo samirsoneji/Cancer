2d93347c38856b972c87faf364cbe2bbeed7d923
samirsoneji <samirsoneji@gmail.com>
2015-03-19 16:51:48 -0400

    example

--- R.code/example.mar.17.2015.R
+++ R.code/example.mar.17.2015.R
@@ -32,8 +32,6 @@ ex.stage <- as.matrix(create.datos.fxn(mx.breast,prop.breast,as.numeric(year.lis
 ex.overall <- c(stage[,1] %*% ex.stage[2,]*0.9,stage[,2] %*% ex.stage[2,]) #*0.9 to create more vertical distance between two bars in panel C
 
 
-breast <- results.fxn(mx.breast, mx.breast.cause, prop.breast, "breast", c(1981,2001))
-
 color <- brewer.pal(6,"YlGnBu")[-c(1,2)]
 stage.list <- c("In Situ","Localized","Regional","Distant")
 width <- 0.2
@@ -41,7 +39,7 @@ offset <- 0.025
 grey.color <- brewer.pal(4,"Greys")[3]
 
 
-pdf("~/Desktop/Cancer/figures/example_decomp1.pdf", height=5.5, width=11, paper="special")
+pdf("~/Desktop/Cancer/figures/example_decomp1a.pdf", height=5.5, width=11, paper="special")
 par (mfrow=c(1,4),mgp=c(2.75,1,0)*0.55,mar=c(1.6,1.5,0.5,1.0)*1.6,omi=c(0.2,0.5,0.4,0), tcl=-0.25,bg="white",cex=0.8,cex.main=0.8)
 
 stage[,2] <- stage[,1]
@@ -83,24 +81,97 @@ dev.off()
 
 
 
+stage <- t(prop.breast[year.list,])
+pdf("~/Desktop/Cancer/figures/example_decomp1b.pdf", height=5.5, width=11, paper="special")
+par (mfrow=c(1,4),mgp=c(2.75,1,0)*0.55,mar=c(1.6,1.5,0.5,1.0)*1.6,omi=c(0.2,0.5,0.4,0), tcl=-0.25,bg="white",cex=0.8,cex.main=0.8)
+
+barplot(100*stage,col=color,border=FALSE,las=1,xaxt="n",yaxt="n")
+xpos <- barplot(100*stage,plot=FALSE)
+axis(1,at=xpos,paste(c("Time 1","Time 2")),tick=FALSE)
+for (i in 1:2) {
+    for (j in c(1,4))
+        text(xpos[i],100*apply(stage,2,function(x) c(x[1]/2,cumsum(x)[-length(cumsum(x))]+(cumsum(x)[-1]-cumsum(x)[-length(cumsum(x))])/2))[j,i],
+             paste(stage.list[j],": ",round(100*stage[j,i]),"%",sep=""),cex=0.8)
+    for (j in c(2,3))
+        text(xpos[i],100*apply(stage,2,function(x) c(x[1]/2,cumsum(x)[-length(cumsum(x))]+(cumsum(x)[-1]-cumsum(x)[-length(cumsum(x))])/2))[j,i],
+             paste(stage.list[j],":\n ",round(100*stage[j,i]),"%",sep=""),cex=0.8)
+}
+
+mx <- mx.breast["75",as.character(c(1981,2001)),]
+mx[2,] <- mx[1,]
+matplot(mx,yaxt="n",xaxt="n",bty="l",pch=19,ylim=c(min(mx),max(mx)),xlim=c(0.85,2.15),cex=2,col=color,ylab=NA,axes=FALSE)
+axis(1,at=1:2,labels=c("Time 1","Time 2"),tick=TRUE)
+matlines(mx,col=color,lty=1,lwd=2)
+text(rep(1.5,4),apply(mx,2,mean)+c(-0.0025,0,0,-0.0025),pos=3,col=color,paste(c("In Situ","Localized","Regional","Distant")))
+
+ex.stage[2,] <- ex.stage[1,]
+barplot(c(t(ex.stage)),space=c(0.1,0.1,0.1,0.1,1,0.1,0.1,0.1),col=color,border=FALSE,las=1,xaxt="n",yaxt="n")
+xpos <- barplot(c(t(ex.stage)),space=c(0.1,0.1,0.1,0.1,1,0.1,0.1,0.1),plot=FALSE)
+text(xpos,ex.stage[2,4]/2,paste(stage.list),srt=90)
+axis(1,at=c(mean(xpos[1:4]),mean(xpos[5:8])),paste(c("Time 1","Time 2")),tick=FALSE)
+
+barplot(ex.overall,border=FALSE,las=1,xaxt="n",yaxt="n",ylim=c(0,max(ex.overall)+5),col=1)
+xpos <- barplot(ex.overall,plot=FALSE)
+axis(1,at=xpos,paste(c("Time 1","Time 2")),tick=FALSE)
+arrows(1.3,ex.overall[1],1.3,ex.overall[2],code=3,length=0.1)
+text(xpos[1],mean(ex.overall)+1,
+     wrap_sentence("Gain in life exp. entirely due to stage shift",12),cex=0.8)
+
+mtext("A. Stage Distribution",side=3,line=0,outer=TRUE,at=1/8,cex=1)
+mtext("B. Stage-Specific Mortality Rates",side=3,line=0,outer=TRUE,at=3/8,cex=1)
+mtext("C. Stage-Specific Life Expectancy",side=3,line=0,outer=TRUE,at=5/8,cex=1)
+mtext("D. Overall Life Expectancy",side=3,line=0,outer=TRUE,at=7/8,cex=1)
+dev.off()
+
+
+prop.breast["2001",] <- prop.breast["1981",]
+mx.breast.cause["other",,"2001","4. distant"] <- (1-0.2) * mx.breast.cause["other",,"2001","4. distant"]
+mx.breast[,"2001","4. distant"] <- mx.breast
+breast <- results.fxn(mx.breast, mx.breast.cause, prop.breast, "breast", c(1981,2001))
+ex <- create.datos.fxn(mx.breast,prop.breast,c(1981,2001))[,"ex.overall"]
+decomp <- unlist(c(breast[12],breast[14],breast[16],breast[18],sum(breast[c(13,15,17,19)])))
+decomp.mat <- as.matrix(cbind(decomp,rep(NA,5)))
+
 pdf("~/Desktop/Cancer/figures/example_decomp2.pdf", height=5.5, width=11, paper="special")
 par (mfrow=c(1,3),mgp=c(2.75,1,0)*0.55,mar=c(1.6,1.5,0.5,1.0)*1.6,omi=c(0.2,0.5,0.4,0), tcl=-0.25,bg="white",cex=0.8,cex.main=0.8)
 
-mx <- mx.breast.cause[,"75",c("1981","2001"),3]
-matplot(t(mx),yaxt="n",xaxt="n",bty="l",pch=19,xlim=c(0.85,2.15),cex=2,col=c("black","grey"),ylab=NA,axes=FALSE)
+mx <- mx.breast.cause[,"75",c("1981","2001"),]
+matplot(t(mx[,,2]),yaxt="n",xaxt="n",bty="l",pch=NA,xlim=c(0.85,2.15),ylab=NA,axes=FALSE,
+        ylim=c(min(mx),max(mx)))
+matpoints(t(mx[,,1]),pch=18,col=color[1])
+matpoints(t(mx[,,2]),pch=19,col=color[2])
+matpoints(t(mx[,,3]),pch=17,col=color[3])
+matpoints(t(mx[,,4]),pch=15,col=color[4])
+matlines(t(mx[,,1]),col=color[1],lty=1:2)
+matlines(t(mx[,,2]),col=color[2],lty=1:2)
+matlines(t(mx[,,3]),col=color[3],lty=1:2)
+matlines(t(mx[,,4]),col=color[4],lty=1:2)
+legend(1.35,0.25,paste(c("Distant, Cancer","Distant, Other","Regional, Cancer","Regional, Other","Localized, Cancer","Localized, Other","In Situ, Cancer","In Situ, Other")),
+       ncol=1,lty=1:2,col=c(color[4],color[4],color[3],color[3],color[2],color[2],color[1],color[1]),
+       text.col=c(color[4],color[4],color[3],color[3],color[2],color[2],color[1],color[1]),
+       pch=c(15,15,17,17,19,19,18,18),bty="n")
 axis(1,at=1:2,labels=c("Time 1","Time 2"),tick=TRUE)
-matlines(t(mx),col=c("black","grey"),lty=1,lwd=2)
-text(1.5,apply(mx,1,mean),c("Cancer","Other Causes"),col=c("black","grey"),pos=3)
 
-barplot(ex.stage[,3],col=1,border=FALSE,las=1,xaxt="n",yaxt="n")
+barplot(ex,col=1,border=FALSE,las=1,xaxt="n",yaxt="n")
 xpos <- barplot(ex.stage[,3],plot=FALSE)
 axis(1,at=xpos,paste(c("Time 1","Time 2")),tick=FALSE)
-arrows(1.3,ex.stage[1,3],1.3,ex.stage[2,3],code=3,length=0.1)
-text(1.3,mean(ex.stage[,3]),"Gain",pos=2)
-   
+arrows(1.3,ex[1],1.3,ex[2],code=3,length=0.1)
+text(1.3,mean(ex),"Gain",pos=2)
+
+barplot(decomp.mat,xlim=c(0,2),ylim=c(0,max(ex)),axes=FALSE,xlab=NA,names.arg=c("Time 2 - Time 1",NA),
+        border=c(color,"darkgrey"),col=c(color,"darkgrey"))
+text(1.6,3,"In Situ, Cancer",col=1)
+arrows(1.45,2,1.2,0.2,col=1,length=0.05)
+text(0.7,1.2,"Localized, Cancer",col="black")
+text(0.7,4.4,"Regional, Cancer",col="black")
+text(1.6,9,"Distant, Cancer",col=1)
+arrows(1.45,7.8,1.2,6,col=1,length=0.05)
+text(0.7,7,"Other Causes",col=1)
+text(1,30,
+     wrap_sentence("Gain in life exp. due to declines in mortality from cancer among in situ patients (2%), declines in mortality from cancer among localized patients (37%), declines in mortality from regional patients (40%), declines in mortality from distant patients (1%), and declines in other-cause mortality (21%)",35))
 
 
-mtext("A. Regional Cancer Mortality Rates",side=3,line=0,outer=TRUE,at=1/6)
+mtext("A. Stage-Specific Cancer Mortality Rates\nBy Cause of Death",side=3,line=0,outer=TRUE,at=1/6)
 mtext("B. Life Expectancy",side=3,line=0,outer=TRUE,at=3/6)
-mtext("C. Cancer PYLL",side=3,line=0,outer=TRUE,at=5/6)
+mtext("C. Gain in Life Expectancy",side=3,line=0,outer=TRUE,at=5/6)
 dev.off()
